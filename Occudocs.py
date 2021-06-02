46bfe1883b2a10aadc71ab00744d43a2a64df655
Kevin Renshaw <kevren22@gmail.com>
2021-06-02 09:37:48 -0400

    re-organize project, create mainpage.py

--- Occudocs.py
+++ Occudocs.py
@@ -1,8 +1,6 @@
-from dash import Dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
-import pandas as pd
-import plotly.graph_objects as go
-import plotly.express as px 
+from mainpage import app
 
+
+
+if __name__ == '__main__':
+    app.run()
