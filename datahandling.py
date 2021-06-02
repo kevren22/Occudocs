566f8472a36f8bb10b7ef1e7038c47d002a65243
Kevin Renshaw <kevren22@gmail.com>
2021-06-02 16:49:56 -0400

    create datahandling.py

---
+++ datahandling.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df2 = pd.read_csv('occupation_Cleaned_1-7.csv').dropna()
+
+df2['MedWage'].replace('>=$208,000',208000,inplace=True)
+
+dfgraph = df2[df2['MedWage'] != '—']
+
+categories = []
--- mainpage.py
+++ mainpage.py
@@ -5,18 +5,11 @@ from dash.dependencies import Input, Output, State
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px 
+from datahandling import df2, dfgraph, categories
 
 BS = 'https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/darkly/bootstrap.min.css'
 
 app = Dash(__name__, external_stylesheets=[BS])
-
-df2 = pd.read_csv('occupation_Cleaned_1-7.csv').dropna()
-
-df2['MedWage'].replace('>=$208,000',208000,inplace=True)
-
-dfgraph = df2[df2['MedWage'] != '—']
-
-categories = []
 
 data = go.Scatter(x=dfgraph['ChgNum'][1:],y=dfgraph['ChgPct'][1:],mode='markers',
                   text=dfgraph['Title'][1:],
@@ -30,6 +23,6 @@ layout = go.Layout(title='Percentage vs Numeric Change of Jobs',
 fig = go.Figure(data=data,layout=layout)
 
 app.layout = html.Div([
-    dcc.Dropdown(id='Category',options=categories,value=),
+    dcc.Dropdown(id='Category',options=categories,value=categories[0]),
     dcc.Graph(id='graph')
 ])
