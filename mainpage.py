7e56e9cf4ed07568fbdc978cb2c8b612d46aa9f4
Kevin Renshaw <kevren22@gmail.com>
2021-06-07 16:53:04 -0400

    add dropdown callback

--- mainpage.py
+++ mainpage.py
@@ -24,6 +24,19 @@ layout = go.Layout(title='Percentage vs Numeric Change of Jobs',
 fig = go.Figure(data=data,layout=layout)
 
 app.layout = html.Div([
-    # dcc.Dropdown(id='Category',options=categories,value=categories[0]),
+    dcc.Dropdown(id='Category',options=categories,value=categories[0]),
     dcc.Graph(id='graph', figure=fig)
 ])
+
+@app.callback(Output('graph', 'figure'),
+              [Input('Category', 'value')])
+def update_category(selected_category):
+    new_data = go.Scatter(x=dfgraph['ChgNum'][1:],y=dfgraph['ChgPct'][1:],mode='markers',
+                  text=dfgraph['Title'][1:],
+                  marker=dict(color=dfgraph['MedWage'][1:].astype(float),colorscale='Jet',
+                              cmin=0, cmid=90000, showscale=True))
+    new_layout = go.Layout(title='Percentage vs Numeric Change of Jobs',
+                   xaxis = {'title':'Change in Number of Jobs (in thousands)'},
+                   yaxis = {'title':'Change in Percentage of Jobs'}
+                   )
+    figure = go.Figure(data=new_data,layout=new_layout)
