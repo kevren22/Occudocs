37cc89e86ea877f4b7d22d0c895abb16ac153c5e
Kevin Renshaw <kevren22@gmail.com>
2021-06-08 11:32:17 -0400

    fix string parsing bug in datahandling

--- datahandling.py
+++ datahandling.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
-df2 = pd.read_csv('occupation_Cleaned_1-7.csv')
+df2 = pd.read_csv('occupation_Cleaned_1-7.csv').dropna()
 df2['MedWage'].replace('>=$208,000',208000,inplace=True)
-dfgraph = df2[df2['MedWage'] != '' and df2['Code'][-2:] != '00']
+dfgraph = df2[df2['MedWage'] != ''][df2['Code'].str[-2:] != '00']
 
 categories = {
     'Front Line Heroes':['Registered nurses', 'Family medicine physicians', 'Firefighters',
                          'Conservation scientists', 'Epidemiologists'],
     '':[]
 }
+
+print(dfgraph)
